215b96cfff535610998051218e08260460871a9e
Carlos José <carlosjo@petco.com>
2019-10-02 19:42:56 -0300

    Initial layout

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="main-header">
+    <img src="./../static/logo.svg" width="50" />
+
+    <h1>Destructuring Game</h1>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+.main-header
+  display: flex
+  align-items: center
+  height: 80px
+  background-color: purple
+  color: white
+  svg
+    width: 80px
+    height: 80px
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,99 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        destructuring-game
-      </h1>
-      <h2 class="subtitle">
-        A funny game to learn the destructuring with javascript
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="content">
+    <div class="workspace">
+      <div class="timer">
+        <h3 class="title">Timer</h3>
+        00:00
+      </div>
+      <div class="score">
+        <h3 class="title">Scores</h3>
+        <div class="player-score">
+          <span>00</span>
+          <span class="name">Player 1</span>
+        </div>
+        <span class="divider">x</span>
+        <div class="player-score">
+          <span>00</span>
+          <span class="name">Player 2</span>
+        </div>
+      </div>
+      <div class="editor">
+        <div class="editor-content"></div>
+        <button>Send</button>
+      </div>
+    </div>
+    <div class="instructions">
+      <h3 class="title">Object view</h3>
+      <div class="object-viewer"></div>
+      <div class="goal">
+        <ul class="goal-list">
+          <li>Value: 'Test'</li>
+          <li>Key: 'AFhSJEI'</li>
+          <li>Index: --</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<style lang="sass" scoped>
+.content
+  display: flex
+  position: relative
+  height: calc(100vh - 80px)
+  .workspace
+    width: 50%
+    height: 100%
+    border-right: 1px solid black
+    font-size: 60px
+    display: flex
+    flex-direction: column
+    padding-top: 80px
+    padding-left: 30px
+    .timer
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
+    .score
+      display: flex
+      flex-wrap: wrap
+      .title
+        width: 100%
+      .divider
+        font-size: 30px
+        margin: 15px 5px
+      .player-score
+        display: flex
+        flex-direction: column
+        align-items: center
+        .name
+          font-size: 14px
+    .editor
+      width: calc(100% - 40px)
+      padding-top: 40px
+      flex-direction: column
+      display: flex
+      align-items: flex-end
+      .editor-content
+        width: 100%
+        height: 260px
+        background: #1f2529
+        margin-bottom: 20px
+      button
+        background: purple
+        color: white
+        border: none
+        padding: 10px 30px
+        font-size: 20px
+        text-transform: uppercase
+        max-width: 150px
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+.instructions
+  position: relative
+  min-width: 50%
+  padding: 30px
+  .title
+    margin-bottom: 10px
+  .object-viewer
+    height: 300px
+    width: 100%
+    border: 1px solid black
 </style>
