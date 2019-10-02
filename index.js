3cb922a440e771b412cd0a955cae743b30499f4d
Victor Chastinet <victorchastinet@live.com>
2019-10-01 21:50:56 -0300

    creating file with a list of names and implemeting the name generator

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+const response = await fetch('https://gist.githubusercontent.com/VChastinet/415c33acc67330b682c655112fb3823b/raw/1645beb6e912ceb8237ec41f766e37fa3af04037/assortedNames.json');
+const nameList = await response.json();
+
 const generateRandomObj = (depth) => Array.from({length: generateRandomNumber(1,3)})
   .map(generateRandomString)
   .reduce((acc, key) => ({ ...acc, [key]: generateRandomValue(depth > 10 && 'primitive', depth + 1)}), {});
 
 const generateRandomArray = (depth) => Array.from({length: generateRandomNumber(0, 10)}).map(() => generateRandomValue(depth > 10 && 'primitive', depth + 1));
 
-const generateRandomString = () => 'asdasdadas'; // TODO: Generate random strings
+const generateRandomString = () => {
+  const randomIndex = Math.round(Math.random() * nameList.length);
+  return nameList[randomIndex];
+};
 
 const generateRandomNumber = (min = 0, max = 100) => Math.floor(Math.random() * (max + 1 - min) + min);
 
 const generateRandomValue = (type, depth) => {
+
   const generatesPrimitives = [
     generateRandomString,
     generateRandomNumber,
@@ -18,24 +25,23 @@ const generateRandomValue = (type, depth) => {
     generateRandomArray,
   ];
 
+  const generates = [...generatesPrimitives, ...generatesComplex];
+  const randomIndex = generateRandomNumber(0, 3);
+  let randomGenerate = generates[randomIndex];
+
   if (type === 'primitive') {
     const randomIndex = generateRandomNumber(0, 1);
-    const randomGenerate = generatesPrimitives[randomIndex];
-    return randomGenerate(depth);
+    randomGenerate = generatesPrimitives[randomIndex];
   }
 
   if (type === 'complex' || depth < 3) {
     const randomIndex = generateRandomNumber(0, 1);
-    const randomGenerate = generatesComplex[randomIndex];
-    return randomGenerate(depth);
-
+    randomGenerate = generatesComplex[randomIndex];
   }
   
-  const generates = [...generatesPrimitives, ...generatesComplex];
-  const randomIndex = generateRandomNumber(0, 3);
-  const randomGenerate = generates[randomIndex];
+
   return randomGenerate(depth);
 };
 
-
-console.log(generateRandomValue('complex', 0));
+const randomValue = generateRandomValue('complex', 0)
+console.log(randomValue);
